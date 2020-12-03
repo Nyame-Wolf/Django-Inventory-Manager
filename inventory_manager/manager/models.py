7138f1cdab636914b9b176640a44d9e37e5a6b1a
Mumenya <mumenyam@gmail.com>
2020-12-03 14:12:58 +0300

    Add Asset m0odel, register it in admin and create migrations

--- inventory_manager/manager/models.py
+++ inventory_manager/manager/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+
+
+class Asset(models.Model):
+    STATUS_CHOICES = (
+        ('assigned', 'Assigned'),
+        ('unassigned', 'UnAssigned'),
+        ('lost', 'Lost'),
+        ('lost & found', 'Lost and Found'),
+    )
+
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=255)
+    serial_no = models.CharField(max_length=25)
+    org_serial_code = models.CharField(max_length=25)
+    status = models.CharField(max_length=50,
+                              choices=STATUS_CHOICES,
+                              default='assigned')
+    date_bought = models.DateTimeField(auto_now_add=True)
+    extra_info = models.TextField()
+
+    def __str__(self):
+        return self.name
