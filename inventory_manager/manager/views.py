994602a2751b6d53bd9154496aad6ef3ca9f393c
Mumenya <mumenyam@gmail.com>
2020-12-08 04:37:52 +0300

    Add manager app urls, link them to the project urls and add the first view

--- inventory_manager/manager/views.py
+++ inventory_manager/manager/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Asset
 
-# Create your views here.
+
+def home(request):
+    context = {
+        'assets': Asset.objects.all()
+    }
+    return render(request, 'manager/list.html',
+                  context)
